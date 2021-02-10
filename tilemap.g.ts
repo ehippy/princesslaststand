7ebdf2a3ff6422cccf5083f8e4bf53144f3458e9
Patrick McDavid <fusionjunky@gmail.com>
2021-02-10 10:15:16 -0700

    Opening line, PyGamer light hookup

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,6 +3,23 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
 
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0a0008000101010101010101010401010101010201010101010401010101010101010101010101010304010101010101010101010101010104010101010101010102010101010101010101010101010101010101`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundSouthWest1,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.darkGroundNorthEast1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
